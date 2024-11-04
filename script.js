dca613f0ee4e272d9ffab721cc10779d41387697
tauheed0110 <tauheed0110@gmail.com>
2024-11-04 15:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const url = location.href;
-alert("The length of the URL is "+url.length);
+// Get the current URL from the location object
+const currentUrl = location.href;
+
+// Calculate the length of the URL
+const urlLength = currentUrl.length;
+
+// Alert the user with the length of the URL
+alert(`The length of the URL is: ${urlLength}`);
